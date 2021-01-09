3ae9eec5a6321e949b9381752589d590fdfe6bf5
Emily Quevedo <37810404+EmilyQue@users.noreply.github.com>
2021-01-09 03:33:44 -0700

    Project setup and navbar

---
+++ resources/views/homePage.blade.php
@@ -0,0 +1,41 @@
+<header>
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Home</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</header>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a href="#" class="navbar-brand">Recipe<b>Book</b></a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- Collection of nav links, forms, and other content for toggling -->
+        <div id="navbarCollapse" class="collapse navbar-collapse justify-content-start">
+            <div class="navbar-nav">
+                <a href="#" class="nav-item nav-link">Home</a>
+                <a href="#" class="nav-item nav-link">Add A Recipe</a>
+                <a href="#" class="nav-item nav-link">About</a>
+            </div>
+
+            <div class="navbar-nav ml-auto">
+                <div class="navbar-nav">
+                    <a href="/test" class="nav-item nav-link"><i class="fa fa-sign-in"></i> Login</a>
+                    <a href="/test" class="nav-item nav-link"><i class="fa fa-user"></i> Register</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+      <div class="container">
+        <h3>Emily's Recipe Book</h3>
+        <p>Recipe book for everyone by everyone.</p>
+      </div>
+</body>
